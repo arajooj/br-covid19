b781afa151d473fd8ea937bf5fce3b1efe8f1c2a
Victor P. Araújo <37886774+VictorArajooj@users.noreply.github.com>
2020-03-19 17:20:32 -0300

    layout mais bonitinho

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api"
+import api2 from "../../services/api2"
 import api3 from "../../services/api3"
 import "./styles.css";
 
@@ -7,6 +8,7 @@ export default class Main extends Component {
 
     state = {
         apiUm: [],
+        apiDois: [],
         apiTres: [],
     }
 
@@ -17,24 +19,39 @@ export default class Main extends Component {
 
     dados = async() => {
         const response = await api.get('/')
-
+        const response2 = await api2.get('/')
         const response3 = await api3.get('/')
 
-        this.setState({ apiUm: response.data})
-        this.setState({ apiTres: response3.data})
+        this.setState({ apiUm:      response.data})
+        this.setState({ apiDois:    response2.data})
+        this.setState({ apiTres:    response3.data})
 
-        console.log(response)
+        //console.log(response2)
     }
 
     render() {
     return (
         <html>
-            <div className="casos-suspeitos">Casos Suspeitos: {this.state.apiUm.suspiciousCases}</div>
-            <div className="testado-n-infectado">Testados Sem Infecção: {this.state.apiUm.testedNotInfected}</div>
-            <div className="infectados">Infectados: {this.state.apiUm.infected}</div>
-            <div className="curados">Curados: {this.state.apiUm.deceased}</div>
-            <div className="mortes">Mortes: {this.state.apiTres.deaths}</div>
+            <div className="Resultados">
 
+                <div className="casos-suspeitos">
+                    Casos Suspeitos: {this.state.apiUm.suspiciousCases}
+                </div>      
+
+                <div className="testado-n-infectado">
+                    Testados Sem Infecção: {this.state.apiUm.testedNotInfected}
+                </div>    
+
+                <div className="infectados">
+                    Infectados: {this.state.apiUm.infected}
+                </div>    
+
+                <div className="curados">
+                    Curados: {this.state.apiUm.deceased}
+                </div>                                     
+
+                <div className="mortes">Mortes: {this.state.apiTres.deaths}</div>                                             
+            </div>
         </html>
         
         
---
+++ frontend/src/services/api2.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api2   = axios.create({ baseURL: 'https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items'});
+
+export default api2
+
+
