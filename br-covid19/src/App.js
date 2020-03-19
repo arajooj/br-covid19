f6c139db23e6c1c6e49168425bdf019e94f32da1
Victor Araujo <contato.vpa@outlook.com>
2020-03-19 01:28:22 -0300

    base

---
+++ br-covid19/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./components/Header";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ br-covid19/src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => <header id="main-header">Brasil COVID-19 Status</header>;
+
+export default Header;
